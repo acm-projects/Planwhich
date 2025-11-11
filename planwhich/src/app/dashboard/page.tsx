c06d2642fb9bd9fec4a772dfc927a625a909d9cb
Kaitlyn <kaitlyn.ma.ferguson@gmail.com>
2025-11-10 22:02:01 -0600

    most updated everything

--- planwhich/src/app/dashboard/page.tsx
+++ planwhich/src/app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import React from 'react';
 import Navbar from '../components/Navbar';
 import TaskStatusBoard from '../components/TaskStatusBoard';
@@ -8,33 +9,25 @@ import FileManager from '../components/FileManager';
 
 export default function DashboardPage() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 pb-6">
       <Navbar />
 
       <div className="p-8">
-        <h1 className="text-3xl font-bold text-gray-900 mb-8">Placeholder Name</h1>
+        <h1 className="text-3xl font-bold text-gray-900 mb-6">Placeholder Name</h1>
 
-        {/* Main layout grid for equal spacing */}
-        <div className="grid grid-cols-3 gap-24 h-[calc(100vh-180px)]">
-          {/* Left Section - Task Status */}
-          <div className="flex flex-col min-h-[400px]">
-            <TaskStatusBoard />
+        {/* 3-column layout */}
+        <div className="grid grid-cols-[2fr_1fr_2fr] gap-6 mb-6">
+          {/* Left Section */}
+          <TaskStatusBoard />
+
+          {/* Middle Section */}
+          <div className="flex flex-col gap-6">
+            <CalendarTasks />
+            <MemberList />
           </div>
 
-          {/* Middle Section - Calendar and Members */}
-          <div className="flex flex-col gap-6 min-h-[400px]">
-            <div className="flex-shrink-0">
-              <CalendarTasks />
-            </div>
-            <div className="flex-1 min-h-0">
-              <MemberList />
-            </div>
-          </div>
-
-          {/* Right Section - My Files */}
-          <div className="flex flex-col min-h-[400px]">
-            <FileManager />
-          </div>
+          {/* Right Section */}
+          <FileManager />
         </div>
       </div>
     </div>
